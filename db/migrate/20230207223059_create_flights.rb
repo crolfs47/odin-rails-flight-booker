69d8b278dd99e84da30dff42aeee4f3f1b870e2a
Cathy Rolfs <crolfs47@gmail.com>
2023-02-07 17:01:58 -0600

    Create Flight model, set up model associations

---
+++ db/migrate/20230207223059_create_flights.rb
@@ -0,0 +1,14 @@
+class CreateFlights < ActiveRecord::Migration[7.0]
+  def change
+    create_table :flights do |t|
+      t.references :departure_airport, null: false, foreign_key: { to_table: :airports }
+      t.references :arrival_airport, null: false, foreign_key: { to_table: :airports }
+      t.datetime :depart_date
+      t.integer :flight_duration
+      t.integer :num_passengers
+
+      t.timestamps
+    end
+
+  end
+end
