0d03427b9de5a23a439dbaaa2305a9394446b35c
Cathy Rolfs <crolfs47@gmail.com>
2023-02-08 14:10:12 -0600

    Add query for departure/arrival airport ids in flights controller, update search form to use date_select

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,16 +1,15 @@
 class FlightsController < ApplicationController
-
-
   def index
     @flights = Flight.all
-    @date_options = Flight.all.map{ |flight| [flight.depart_date.strftime("%m/%d/%Y")]}
-    @airport_options = Airport.all.map{ |airport| [airport.code, airport.id]}
-    @flight_options = Flight.all.where(departure_airport_id: params[:departure_airport_id])
+    @date_options = Flight.all.map { |flight| [flight.depart_date_formatted] }
+    @airport_options = Airport.all.map { |airport| [airport.code, airport.id] }
+    @available_flights = Flight.all.where(departure_airport_id: params[:departure_airport_id]).where(arrival_airport_id: params[:arrival_airport_id])#.where(depart_date_formatted: params[:depart_date])
   end
 
   private
 
   def post_params
-    params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :depart_date, :flight_duration, :num_passengers)
+    params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :depart_date, :flight_duration,
+                                   :num_passengers)
   end
 end
