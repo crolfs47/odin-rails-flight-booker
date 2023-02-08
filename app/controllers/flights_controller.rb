bb8a0e27d69aa768d01bbd410357c3f47f1a0e6a
Cathy Rolfs <crolfs47@gmail.com>
2023-02-08 16:16:54 -0600

    Add find_flights method to Flights controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,11 +3,21 @@ class FlightsController < ApplicationController
     @flights = Flight.all
     @date_options = Flight.all.map { |flight| [flight.depart_date_formatted] }
     @airport_options = Airport.all.map { |airport| [airport.code, airport.id] }
-    @selected_date = Date.new(params['depart_date(1i)'].to_i, params['depart_date(2i)'].to_i, params['depart_date(3i)'].to_i,)
-    @available_flights = Flight.all.where(departure_airport_id: params[:departure_airport_id]).where(arrival_airport_id: params[:arrival_airport_id]).where("depart_date = ?", @selected_date)
+    @available_flights = find_flights
+    # if params
+    #   @selected_date = Date.new(params['depart_date(1i)'].to_i, params['depart_date(2i)'].to_i, params['depart_date(3i)'].to_i)
+    #   @available_flights = Flight.all.where(departure_airport_id: params[:departure_airport_id]).where(arrival_airport_id: params[:arrival_airport_id]).where("depart_date = ?", @selected_date)
+    # end
   end
 
   private
+
+  def find_flights
+    return [] unless params['depart_date(1i)'].present?
+
+    date = Date.new(params['depart_date(1i)'].to_i, params['depart_date(2i)'].to_i, params['depart_date(3i)'].to_i)
+    Flight.all.where(departure_airport_id: params[:departure_airport_id]).where(arrival_airport_id: params[:arrival_airport_id]).where("depart_date = ?", date)
+  end
 
   def post_params
     params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :depart_date, :flight_duration,
