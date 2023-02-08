44dd0bcf234c6e9c2a54d7409ff9caea19530e2e
Cathy Rolfs <crolfs47@gmail.com>
2023-02-08 11:07:34 -0600

    Set up search form on Flights index page

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,15 @@
 class FlightsController < ApplicationController
+
+
   def index
+    @flights = Flight.all
+    @flight_dates = @flights.map{ |flight| [flight.depart_date.strftime("%m/%d/%Y"), flight.id]}
+    @airport_options = Airport.all.map{ |airport| [airport.code, airport.id]}
+  end
+
+  private
+
+  def post_params
+    params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :depart_date, :flight_duration, :num_passengers)
   end
 end
