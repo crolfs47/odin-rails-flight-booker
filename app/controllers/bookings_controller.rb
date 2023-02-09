9144ce4cd8a0febdaa98ae0c43f17a5365212642
Cathy Rolfs <crolfs47@gmail.com>
2023-02-09 17:11:48 -0600

    Add form to #new bookings view using #fields_for to handle both booking and passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,18 @@
 class BookingsController < ApplicationController
   def new
-    
-    @flight_id = params[:flight_id]
-    @flight = Flight.find(@flight_id)
-    @passenger = Passenger.new
     @booking = Booking.new
+
+    @flight = Flight.find(params[:flight_id])
+    
     @num_passengers = params[:num_passengers].to_i
     @num_passengers.times { @booking.passengers.build }
   end
 
   def create 
+  end
+
+  private 
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:first_name, :last_name, :email])
   end
 end
