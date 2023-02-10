9261516cef8111eafab8fb2dffd707e75e56b9ef
Cathy Rolfs <crolfs47@gmail.com>
2023-02-10 16:09:06 -0600

    Add CSS styling for index and flights form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,10 +4,16 @@ class FlightsController < ApplicationController
     # @date_options = Flight.all.map { |flight| [flight.depart_date_formatted] }
     @airport_options = Airport.all.map { |airport| [airport.code, airport.id] }
     @available_flights = find_flights
-
   end
 
   private
+
+  def format_duration(duration)
+    hrs = duration / 60
+    min = duration % 60
+    min = format('%02d',min)
+    return "#{hrs}:#{min}"
+  end
 
   def find_flights
     return [] unless params['depart_date(1i)'].present?
@@ -16,7 +22,7 @@ class FlightsController < ApplicationController
     Flight.all.where(departure_airport_id: params[:departure_airport_id]).where(arrival_airport_id: params[:arrival_airport_id]).where("depart_date = ?", @selected_date)
   end
 
-  def post_params
+  def flights_params
     params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :depart_date, :flight_duration,
                                    :num_passengers)
   end
