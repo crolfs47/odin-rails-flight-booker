77bb9b20b50e94e717c5dd5d34894065992e1e9f
Cathy Rolfs <crolfs47@gmail.com>
2023-02-28 09:48:46 -0600

    Add confirmation email method

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'notifications@flightbooker.com'
+
+  def confirm_email
+    @passenger = params[:passenger]
+    mail(to: @passenger.email, subject: 'Flight Confirmation')
+  end
+  
 end
