1c1dc9d31c304673d18e138ce2e3501631a8a378
Cathy Rolfs <crolfs47@gmail.com>
2023-02-28 11:26:01 -0600

    Add more detail to mailer html view

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -4,8 +4,9 @@ class PassengerMailer < ApplicationMailer
   def confirm_email
     @booking = params[:booking]
     @passengers = @booking.passengers
+    @flight = @booking.flight
     emails = @passengers.map { |x| x[:email] }
-    mail(to: emails, subject: 'Flight Confirmation')
+    mail(to: emails, subject: "Flight Confirmation for Flight ##{@booking.flight_id}")
   end
 
 end
