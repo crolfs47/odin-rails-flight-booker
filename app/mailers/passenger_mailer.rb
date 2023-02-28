2137dec3dced2d0704099ca55e32635473b07dc7
Cathy Rolfs <crolfs47@gmail.com>
2023-02-28 12:38:48 -0600

    Add link to booking in mailer view

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -5,6 +5,7 @@ class PassengerMailer < ApplicationMailer
     @booking = params[:booking]
     @passengers = @booking.passengers
     @flight = @booking.flight
+    @url = booking_url(@booking, host: 'localhost:3000')
     emails = @passengers.map { |x| x[:email] }
     mail(to: emails, subject: "Flight Confirmation for Flight ##{@booking.flight_id}")
   end
